134781ae2442604f5a5b8ef561ba6ea8715d6784
odanchen <91546290+odanchen@users.noreply.github.com>
2023-12-29 14:36:40 -0500

    bitmasking added

--- core/src/com/totallynotacult/jam/map/Tile.java
+++ core/src/com/totallynotacult/jam/map/Tile.java
@@ -2,12 +2,18 @@ package com.totallynotacult.jam.map;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Tile extends Sprite {
     protected final int SIZE = 16;
     public boolean weaponTile = false;
 
     public Tile(Texture texture, int row, int col) {
+        super(texture);
+        setBounds(col * SIZE, row * SIZE, SIZE, SIZE);
+    }
+
+    public Tile(TextureRegion texture, int row, int col) {
         super(texture);
         setBounds(col * SIZE, row * SIZE, SIZE, SIZE);
     }
--- core/src/com/totallynotacult/jam/map/Wall.java
+++ core/src/com/totallynotacult/jam/map/Wall.java
@@ -1,9 +1,14 @@
 package com.totallynotacult.jam.map;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Wall extends Tile {
     public Wall(Texture texture, int row, int col) {
         super(texture, row, col);
+    }
+
+    public Wall(TextureRegion textureRegion, int row, int col) {
+        super(textureRegion, row, col);
     }
 }
